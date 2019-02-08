b1c158811b5cd23869cf911cfef5687de5f4c3af
Scott Bailey <scott@scottbailey.net>
2019-02-08 21:15:31 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import "./App.css";
+import { recipes } from "./tempList";
+import RecipeList from "./components/RecipeList";
+import RecipeDetails from "./components/RecipeDetails";
 
 class App extends Component {
+  state = {
+    recipes: [],
+    url:
+      "https://www.food2fork.com/api/search?key=1af908314527e129a0a5951522ab31c0"
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <RecipeList />
+        <RecipeDetails />
+      </React.Fragment>
     );
   }
 }
---
+++ src/components/Recipe.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Recipe extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello From Recipe</h1>
+      </React.Fragment>
+    );
+  }
+}

588f6cc8dff530e41c81e0f2453b8c92c5ab68b0
Scott Bailey <scott@scottbailey.net>
2019-02-09 15:21:11 +1000

    Show list of recipes

--- src/App.js
+++ src/App.js
@@ -7,15 +7,33 @@ import RecipeDetails from "./components/RecipeDetails";
 
 class App extends Component {
   state = {
-    recipes: [],
+    recipes: recipes,
     url:
       "https://www.food2fork.com/api/search?key=1af908314527e129a0a5951522ab31c0"
   };
 
+  // async getRecipes() {
+  //   try {
+  //     const data = await fetch(this.state.url);
+  //     const jsonData = await data.json();
+
+  //     this.setState({
+  //       recipes: jsonData.recipes
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  // componentDidMount() {
+  //   this.getRecipes();
+  // }
+
   render() {
+    // console.log(this.state.recipes);
     return (
       <React.Fragment>
-        <RecipeList />
+        <RecipeList recipes={this.state.recipes} />
         <RecipeDetails />
       </React.Fragment>
     );
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -4,11 +4,24 @@ import RecipeSearch from "./RecipeSearch";
 
 export default class RecipeList extends Component {
   render() {
+    const { recipes } = this.props;
     return (
       <React.Fragment>
-        <h1>Hello from List</h1>
         <RecipeSearch />
-        <Recipe />
+        <div className="container my-5">
+          {/* title */}
+          <div className="row">
+            <div className="col-10 mx-auto col-md-6 text-center text-uppercase mb-3">
+              <h1 className="text-slanted">Recipe List</h1>
+            </div>
+          </div>
+          {/* end of title */}
+          <div className="row">
+            {recipes.map(recipe => {
+              return <Recipe key={recipe.recipe_id} recipe={recipe} />;
+            })}
+          </div>
+        </div>
       </React.Fragment>
     );
   }
